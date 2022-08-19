bd83ed5d4afa9593ba8b148460a2e584e34fd5d5
Ibyaheem <ha585115@gmail.com>
2022-08-19 04:22:29 +0200

    Login

---
+++ lib/model/login_model.dart
@@ -0,0 +1,50 @@
+class LoginUserModel {
+  late String name;
+  late String email;
+  late String password;
+  late String uId;
+
+  LoginUserModel({
+    required this.email,
+    required this.name,
+    required this.password,
+    required this.uId,
+  });
+
+  LoginUserModel.fromJson(Map<String, dynamic> json) {
+    email = json['email'];
+    name = json['name'];
+    password = json['password'];
+    uId = json['uId'];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'email': email,
+      'password': password,
+      'uId': uId,
+    };
+  }
+}
+
+class CachHelper {
+  late String key;
+  late String value;
+
+  CachHelper({
+    required this.key,
+    required this.value,
+  });
+  CachHelper.fromJson(Map<String, dynamic> json) {
+    key = json['key'];
+    value = json['value'];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'key': key,
+      'value': value,
+    };
+  }
+}
