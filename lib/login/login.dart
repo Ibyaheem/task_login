19869160635c264020d419b143e0b8a32c2aa193
Ibrahim L Badwy <87869566+Ibyaheem@users.noreply.github.com>
2023-01-08 00:19:16 +0200

    Update login.dart

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,391 +1,147 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
-import '../model/login_model.dart';
 
-class LoginScreen extends StatefulWidget {
+class Login extends StatefulWidget {
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<Login> createState() => _LoginState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
-  hexColor(String colorhexcode) {
-    String colornew = '0xff' + colorhexcode;
-    colornew = colornew.replaceAll('#', '');
-    int colorint = int.parse(colornew);
-    return colorint;
-  }
-
+class _LoginState extends State<Login> {
   var emailController = TextEditingController();
-  var usernameController = TextEditingController();
+
   var passwordController = TextEditingController();
-  bool isClicked = false;
+
   var formKey = GlobalKey<FormState>();
 
   bool isPassword = true;
 
-  late FirebaseAuth mAuth;
-  setData() async {
-    var user = await FirebaseAuth.instance.currentUser;
-    var gett = await FirebaseFirestore.instance
-        .collection("users")
-        .doc(user!.uid)
-        .set({
-      'email': emailController.text,
-      'password': passwordController.text,
-      'name': usernameController.text,
-      'uId': user.uid,
-    });
-  }
-
-  String user_name = 'loading name ........';
-  String user_email = 'loading email ........';
-  String user_uid = 'loading email ........';
-
-  GetData() async {
-    var user = await FirebaseAuth.instance.currentUser;
-    var gett = await FirebaseFirestore.instance
-        .collection("users")
-        .doc(user!.uid)
-        .get();
-    if (this.mounted) {
-      setState(() {
-        user_name = gett.data()!['name'];
-        user_email = gett.data()!['email'];
-        user_uid = gett.data()!['uId'];
-      });
-    }
-  }
-
+  //key => unique id to a specific widget
   @override
   Widget build(BuildContext context) {
-    //final user=UserPreferences.getUser();
-    GetData();
     return Scaffold(
-      body: SafeArea(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsetsDirectional.only(top: 30.0),
-                  child: Image(
-                    image: AssetImage('assets/smart-home(2).png'),
-                    width: 50.0,
-                    height: 50.0,
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Text(
-                  'Welcome back !',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Padding(
-                  padding: const EdgeInsetsDirectional.only(top: 20.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(50.0),
-                        topRight: Radius.circular(50.0),
-                      ),
-                      color: Color(hexColor("#264653")),
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
                     ),
+                  ),
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                  TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelText: 'Email Address',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(
+                        Icons.email,
+                      ),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    onFieldSubmitted: (value) {
+                      print(value);
+                    },
+                    onChanged: (value) {
+                      print(value);
+                    },
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'email address must not be empty';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(
+                        Icons.lock,
+                      ),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isPassword = !isPassword;
+                          });
+                        },
+                        icon: Icon(
+                          isPassword ? Icons.visibility : Icons.visibility_off,
+                        ),
+                      ),
+                    ),
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: isPassword,
+                    onFieldSubmitted: (value) {
+                      print(value);
+                    },
+                    onChanged: (value) {
+                      print(value);
+                    },
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'password must not be empty';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Container(
                     width: double.infinity,
-                    child: Form(
-                      key: formKey,
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 30.0,
-                            ),
-                            child: Padding(
-                              padding:
-                                  const EdgeInsetsDirectional.only(top: 50.0),
-                              child: TextFormField(
-                                cursorColor: Colors.white,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
-                                controller: emailController,
-                                keyboardType: TextInputType.name,
-                                onFieldSubmitted: (value) {
-                                  print(value);
-                                },
-                                onChanged: (value) {
-                                  print(value);
-                                },
-                                validator: (value) {
-                                  if (value!.isEmpty) {
-                                    return 'email must not be empty';
-                                  } else
-                                    return null;
-                                },
-                                decoration: InputDecoration(
-                                  labelText: 'Email',
-                                  labelStyle: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                  prefixIcon: Icon(
-                                    Icons.email,
-                                    color: Colors.white,
-                                  ),
-                                  border: OutlineInputBorder(
-                                    borderSide: const BorderSide(
-                                        width: 3, color: Colors.blue),
-                                    borderRadius: BorderRadius.circular(15),
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                    borderSide: const BorderSide(
-                                        width: 3, color: Colors.white),
-                                    borderRadius: BorderRadius.circular(15),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 15.0,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 30.0,
-                            ),
-                            child: TextFormField(
-                              cursorColor: Colors.white,
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                              controller: passwordController,
-                              keyboardType: TextInputType.visiblePassword,
-                              obscureText: isPassword,
-                              onFieldSubmitted: (value) {
-                                print(value);
-                              },
-                              onChanged: (value) {
-                                print(value);
-                              },
-                              validator: (value) {
-                                if (value!.isEmpty) {
-                                  return 'password is too short! ';
-                                } else
-                                  return null;
-                              },
-                              decoration: InputDecoration(
-                                labelText: 'Password',
-                                labelStyle: TextStyle(
-                                  color: Colors.white,
-                                ),
-                                focusColor: Colors.white,
-                                prefixIcon: Icon(
-                                  Icons.lock,
-                                  color: Colors.white,
-                                ),
-                                suffixIcon: IconButton(
-                                  icon: Icon(
-                                    isPassword
-                                        ? Icons.visibility
-                                        : Icons.visibility_off,
-                                    color: Colors.white,
-                                  ),
-                                  onPressed: () {
-                                    setState(() {
-                                      isPassword = !isPassword;
-                                    });
-                                  },
-                                ),
-                                border: OutlineInputBorder(
-                                  borderSide: const BorderSide(
-                                      width: 3, color: Colors.blue),
-                                  borderRadius: BorderRadius.circular(15),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: const BorderSide(
-                                      width: 3, color: Colors.white),
-                                  borderRadius: BorderRadius.circular(15),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              TextButton(
-                                onPressed: () {},
-                                child: Text(
-                                  'Forgot Password?',
-                                  style: TextStyle(
-                                    fontSize: 15.0,
-                                    color: Colors.amber,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 100.0,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 50.0,
-                            ),
-                            child: Container(
-                              width: double.infinity,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(
-                                  25.0,
-                                ),
-                              ),
-                              child: MaterialButton(
-                                onPressed: () async {
-                                  if (formKey.currentState!.validate()) {
-                                    setState(() {
-                                      isClicked = true;
-                                    });
-                                    var result = await FirebaseAuth.instance
-                                        .signInWithEmailAndPassword(
-                                            email: emailController.text,
-                                            password: passwordController.text);
-                                    if (result != null) {
-                                      GetData();
-                                    }
-                                    showToast(
-                                        text: 'Logged in Successfully',
-                                        state: ToastStates.SUCCESS);
-                                  } else {
-                                    showToast(
-                                        text: 'Please, type your info',
-                                        state: ToastStates.ERROR);
-                                  }
-                                },
-                                child: Text(
-                                  'Login',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Color(hexColor("#264653")),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 100.0,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'You don\'t have an account?',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
-                              ),
-                              TextButton(
-                                onPressed: () {},
-                                child: Text(
-                                  'Sign Up',
-                                  style: TextStyle(
-                                    color: Colors.amber,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 87.0,
-                          )
-                        ],
+                    color: Colors.blue,
+                    child: MaterialButton(
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          print(emailController.text);
+                          print(passwordController.text);
+                        }
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('don\'t have an account'),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Register Now',
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
   }
-
-  void userCreate({
-    required String name,
-    required String email,
-    required String password,
-    required String uId,
-  }) {
-    LoginUserModel model = LoginUserModel(
-      email: email,
-      name: name,
-      password: password,
-      uId: uId,
-    );
-    FirebaseFirestore.instance
-        .collection('users')
-        .doc(uId)
-        .set({'success': 'success'})
-        .then((value) {})
-        .catchError((error) {
-          print(error.toString());
-        });
-  }
-
-  void userLogin({
-    required String email,
-    required String password,
-  }) {
-    FirebaseAuth.instance
-        .signInWithEmailAndPassword(
-      email: email,
-      password: password,
-    )
-        .then((value) {
-      print(value.user?.uid);
-      if (value.user?.uid == true) {
-        print('Log in Success');
-      }
-    }).catchError((error) {
-      print(error.toString());
-    });
-  }
-}
-
-void showToast({required String text, required ToastStates state}) =>
-    Fluttertoast.showToast(
-        msg: text,
-        toastLength: Toast.LENGTH_LONG,
-        gravity: ToastGravity.BOTTOM,
-        timeInSecForIosWeb: 1,
-        backgroundColor: chooseToastColor(state),
-        textColor: Colors.white,
-        fontSize: 16.0);
-
-enum ToastStates { SUCCESS, ERROR, WARNING }
-
-Color chooseToastColor(ToastStates state) {
-  Color color;
-  switch (state) {
-    case ToastStates.SUCCESS:
-      color = Colors.green;
-      break;
-    case ToastStates.ERROR:
-      color = Colors.red;
-      break;
-    case ToastStates.WARNING:
-      color = Colors.amber;
-      break;
-  }
-  return color;
 }
